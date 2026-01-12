7849a6a47a1b393344656a8ecda3c6d6db778d9b
Evo <gcevocastanares@gmail.com>
2026-01-12 11:11:55 +0800

    evo4

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,22 @@ import Test from './pages/test/Test'
 import Layout from './layout/Layout'
 import DashboardRedirect from './context/DashboardRedirect'
 
+
+import Category from './pages/category/Category'
+import Brand from './pages/brand/Brand'
+import Units from './pages/unit/Units'
+import Office from './pages/office/Office'
+import Acquisitions from './pages/acquisitions/Acquisitions'
+import Items from './pages/items/Items'
 function App() {
   return (
-    
       <BrowserRouter>
-         
         <Routes>
           {/* Public route */}
           <Route path="/" element={<LoadPage />} />
           <Route path="/login" element={<LogInPage />} />
           <Route path="/test" element={<Test />} />
+           <Route  path="*" element={<div>404 Not Found</div>} />
           {/* Protected / App layout */}
           <Route  element={<Layout />}>
             <Route
@@ -27,6 +33,14 @@ function App() {
                 // </DashboardRedirect>
               }
             />
+            
+            <Route path="/categories" element={<Category />} />
+            <Route path='/brands' element={<Brand />} />
+            <Route path='/units' element={<Units />} />
+            <Route path='/offices' element={<Office />} />
+            <Route path='/acquisitions' element={<Acquisitions />} />
+
+            <Route path="/items" element={<Items />} />
           </Route>
         </Routes>
       </BrowserRouter>
